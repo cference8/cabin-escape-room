96b4fb37527f429c0dde333b582d3692fdf9befb
Chris <cference8@gmail.com>
2022-11-11 20:50:42 -0600

    initial commit

---
+++ src/main/java/com/cf/cabinescaperoom/CabinEscapeRoomApplication.java
@@ -0,0 +1,13 @@
+package com.cf.cabinescaperoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinEscapeRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinEscapeRoomApplication.class, args);
+	}
+
+}
