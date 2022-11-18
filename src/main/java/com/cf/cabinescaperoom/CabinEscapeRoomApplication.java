c3dac9fc14033d9a1849783a6d2677119c1db1ee
Chris <cference8@gmail.com>
2022-11-18 14:58:12 -0600

    Jasypt Encryption Complete. Time to Deploy.

--- src/main/java/com/cf/cabinescaperoom/CabinEscapeRoomApplication.java
+++ src/main/java/com/cf/cabinescaperoom/CabinEscapeRoomApplication.java
@@ -1,9 +1,11 @@
 package com.cf.cabinescaperoom;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class CabinEscapeRoomApplication {
 
 	public static void main(String[] args) {
